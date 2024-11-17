063a4f28640f0e59a8bb155391564dbab93f1ad5
MazenTayseer <maziiin360@yahoo.com>
2024-11-17 14:15:21 +0200

    added getters and setters by hand in base donation

--- src/main/java/com/example/hospital/models/BaseDonation.java
+++ src/main/java/com/example/hospital/models/BaseDonation.java
@@ -5,12 +5,8 @@ import java.util.Date;
 import com.example.hospital.services.factory.IDonation;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class BaseDonation implements IDonation {
     @Id
@@ -20,7 +16,7 @@ public abstract class BaseDonation implements IDonation {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "donor_id", nullable = false)
     private Donor donor;
-    
+
     @Column(nullable = false)
     private float amount;
 
@@ -28,14 +24,50 @@ public abstract class BaseDonation implements IDonation {
     private Date date;
 
     @Column(nullable = false)
-    private String type; 
+    private String type;
 
-    public BaseDonation (){}
+    public BaseDonation() {}
 
     public BaseDonation(Donor donor, float amount, Date date, String type) {
         this.donor = donor;
         this.amount = amount;
         this.date = date;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Donor getDonor() {
+        return donor;
+    }
+
+    public void setDonor(Donor donor) {
+        this.donor = donor;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
         this.type = type;
     }
 
