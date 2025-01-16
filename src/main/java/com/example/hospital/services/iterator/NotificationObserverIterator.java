7faf88c1af5499fbb10d4ee2f924000b7f118f7d
Adham Amr <adhamamr360@yahoo.com>
2025-01-16 16:57:27 +0200

    Iterator 2

--- src/main/java/com/example/hospital/services/iterator/NotificationObserverIterator.java
+++ src/main/java/com/example/hospital/services/iterator/NotificationObserverIterator.java
@@ -1,5 +1,27 @@
 package com.example.hospital.services.iterator;
 
-public class NotificationObserverIterator {
-    
+import java.util.List;
+import com.example.hospital.services.observer.notifications.INotificationObserver;
+
+public class NotificationObserverIterator implements Iterator<INotificationObserver> {
+    private final List<INotificationObserver> observers;
+    private int position;
+
+    public NotificationObserverIterator(List<INotificationObserver> observers) {
+        this.observers = observers;
+        this.position = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < observers.size();
+    }
+
+    @Override
+    public INotificationObserver next() {
+        if (!hasNext()) {
+            throw new IllegalStateException("No more elements");
+        }
+        return observers.get(position++);
+    }
 }
--- src/main/java/com/example/hospital/services/iterator/UserIterator.java
+++ src/main/java/com/example/hospital/services/iterator/UserIterator.java
@@ -1,5 +1,27 @@
 package com.example.hospital.services.iterator;
 
-public class UserIterator {
-    
+import java.util.List;
+import com.example.hospital.models.User;
+
+public class UserIterator implements Iterator<User> {
+    private final List<User> users;
+    private int position;
+
+    public UserIterator(List<User> users) {
+        this.users = users;
+        this.position = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < users.size();
+    }
+
+    @Override
+    public User next() {
+        if (!hasNext()) {
+            throw new IllegalStateException("No more elements");
+        }
+        return users.get(position++);
+    }
 }
